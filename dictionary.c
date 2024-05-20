e06fc0782749483be0cc186f586553e402cdb6e6
Raj <155278668+rkalaa@users.noreply.github.com>
2024-05-19 22:16:29 -0400

    Add files via upload

---
+++ dictionary.c
@@ -0,0 +1,75 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
+unsigned int word_count;
+unsigned int hash_value;
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO: Improve this hash function
+    return toupper(word[0]) - 'A';
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)// error check
+    {
+        printf("Can not read dict\n");
+        return NULL;
+    }
+    char word[LENGTH + 1];
+    while(fscanf(file, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        return false;
+        strcpy(n->word, word);
+        hash_value = hash(word);
+        table[hash_value] = n;
+        word_count++;
+
+    }
+    fclose(file);
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
